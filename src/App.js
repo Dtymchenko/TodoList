f59b6dd9c2424d5bf98520a3b9978853ac3ebed0
Dmytro Tymchenko <timchenkod88@gmail.com>
2022-09-30 19:45:43 +0300

    fixed filter bug and added localStorage

--- src/App.js
+++ src/App.js
@@ -5,23 +5,45 @@ import React from "react";
 
 function App() {
 
-  const [items, setItems] = React.useState([])
-  const [filtered, setFiltered] = React.useState([])
+  const isDefaultItems = () => {
+    let defaultItems = localStorage.getItem("items")
+    if (defaultItems) {
+      return JSON.parse(localStorage.getItem("items"))
+    } else {
+      return []
+    }
+  }
 
-  // React.useEffect(() => {
-  //   setFiltered(items)
-  // }, [items])
+  const [items, setItems] = React.useState(isDefaultItems())
+  const [filtered, setFiltered] = React.useState([])
+  const [filter, setFilter] = React.useState('')
+
+  const getFilterFromSelect = (str) => {
+    setFilter(str)
+  }
+
+  React.useEffect(() => {
+    setFiltered(items)
+    localStorage.setItem("items", JSON.stringify(items))
+    if (filter === "Active") {
+      setFiltered(items.filter((item) => item.isDone === false))
+    } else if (filter === "Done") {
+      setFiltered(items.filter((item) => item.isDone === true))
+    }
+  }, [items])
 
   return (
     <div className="App min-h-screen text-3xl w-[900px] mx-auto bg-gradient-to-r from-indigo-500 to-pink-500">
       <Header />
       <div className="flex justify-center text-lime-500 mt-5 font-bold">Total tasks: {items.length}</div>
-      <div className="flex justify-center text-lime-500 mt-5 font-bold">Filtered tasks: {filtered.length}</div>
+      {/* <div className="flex justify-center text-lime-500 mt-5 font-bold">Filtered tasks: {filtered.length}</div> */}
       <Input
       items={items}
       setItems={setItems}
       filtered={filtered}
       setFiltered={setFiltered}
+      getFilterFromSelect={getFilterFromSelect}
+      isDefaultItems={isDefaultItems}
       />
       <Item
       items={items}
